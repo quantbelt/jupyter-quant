16d58c49e8b8b3d772a7539eef5b6fe8df6b8583
gnzsnz <8376642+gnzsnz@users.noreply.github.com>
2023-08-30 21:42:54 +0200

    2308.06 release (#4)
    * set pip environment variables (https://pip.pypa.io/en/stable/topics/configuration/#environment-variables)
    
    set PIP_USER=true in Dockerfile -
    set PIP_REQUIRE_VIRTUALENV= in ´.env-dist´
    manage PIP_REQUIRE_VIRTUALENV in ´entrypoint.sh`
    
    * set cache on docker build
    
    * bump version
    
    * add message for apt proxy
    
    * add ipympl and widgets

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ DAEMON=jupyter-lab
 
 # APT Proxy Cache
 if [ -n "${APT_PROXY}" ]; then
+    echo "> Setting apt proxy 📌"
     echo "Acquire::http { Proxy "'$APT_PROXY'"; }"  \
       | sudo tee /etc/apt/apt.conf.d/01proxy
 fi;
@@ -34,8 +35,15 @@ if [ ! -L "${JUPYTER_SERVER_ROOT}"/.lsp_symlink ]; then
     ln -s / .lsp_symlink
 fi;
 
+if [[ -n "$PIP_REQUIRE_VIRTUALENV" ]]; then
+    echo "PIP_REQUIRE_VIRTUALENV 📌 set to: $PIP_REQUIRE_VIRTUALENV"
+else
+    PIP_REQUIRE_VIRTUALENV=false
+    export PIP_REQUIRE_VIRTUALENV=false
+fi
+
 stop() {
-    echo "> Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    echo "> 😘 Received SIGINT or SIGTERM. Shutting down $DAEMON"
     # Get PID
     local pid
     pid=$(cat /tmp/$DAEMON.pid)
