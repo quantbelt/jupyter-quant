ce28235a3a6c9c230f2461680dfbc57492435847
gnzsnz <gonzalosaenz@gmail.com>
2023-11-19 19:39:53 +0100

    implement pre-commit (#55)
    * implement pre-commit
    
    lint files with pre-commit hooks
    
    * align github workflows with IMG_PYTHON_VERSION variable
    Changes to be committed:
    	modified:   .github/workflows/docker-base-image.yml
    	modified:   .github/workflows/docker-build-n-test.yml
    	modified:   .github/workflows/docker-publish.yml
    
    * setup .hadolint.yaml
    
    Changes to be committed:
    	new file:   .hadolint.yaml
    	modified:   .pre-commit-config.yaml
    
    * run markdown lint in README.md
    Changes to be committed:
    	modified:   README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,27 +15,26 @@ DAEMON=jupyter-lab
 
 # APT Proxy Cache
 if [ -n "${APT_PROXY}" ]; then
-    echo "> Setting apt proxy 📌"
-    echo "Acquire::http { Proxy "'$APT_PROXY'"; }"  \
-      | sudo tee /etc/apt/apt.conf.d/01proxy
-fi;
+	echo "> Setting apt proxy 📌"
+	echo "Acquire::http { Proxy \"${APT_PROXY}\"; }" |
+		sudo tee /etc/apt/apt.conf.d/01proxy
+fi
 
 # dotfiles
 if [ -d "$BYODF" ]; then
-    echo "> setting dotfiles 📌 at $BYODF"
-    stow --adopt -t "$HOME" -d "$(dirname "$BYODF")" "$(basename "$BYODF")"
-    git -C "$BYODF" reset --hard 1> /dev/null
-fi;
+	echo "> setting dotfiles 📌 at $BYODF"
+	stow --adopt -t "$HOME" -d "$(dirname "$BYODF")" "$(basename "$BYODF")"
+	git -C "$BYODF" reset --hard 1>/dev/null
+fi
 
 # ssh keys
 if [ -d "${SSH_KEYDIR}" ]; then
-    if [ ! -L /home/"${USER}"/.ssh ]
-    then
-        echo "> Setting SSH key 🔑 at $SSH_KEYDIR"
-        ln -s "${SSH_KEYDIR}" /home/"${USER}"/.ssh
-    else
-        echo "> Setting SSH key 🔑: keys already exists /home/${USER}/.ssh"
-    fi
+	if [ ! -L /home/"${USER}"/.ssh ]; then
+		echo "> Setting SSH key 🔑 at $SSH_KEYDIR"
+		ln -s "${SSH_KEYDIR}" /home/"${USER}"/.ssh
+	else
+		echo "> Setting SSH key 🔑: keys already exists /home/${USER}/.ssh"
+	fi
 fi
 
 # jupyterlab-lsp
@@ -43,20 +42,20 @@ JUPYTER_OPT='--ContentsManager.allow_hidden=True'
 
 # language server symlink
 if [ ! -L "${JUPYTER_SERVER_ROOT}"/.lsp_symlink ]; then
-    ln -s / .lsp_symlink
-fi;
+	ln -s / .lsp_symlink
+fi
 
 stop() {
-    echo "> 😘 Received SIGINT or SIGTERM. Shutting down $DAEMON"
-    # Get PID
-    local pid
-    pid=$(cat /tmp/$DAEMON.pid)
-    # Set TERM
-    kill -SIGTERM "${pid}"
-    # Wait for exit
-    wait "${pid}"
-    # All done.
-    echo "> Done... $?"
+	echo "> 😘 Received SIGINT or SIGTERM. Shutting down $DAEMON"
+	# Get PID
+	local pid
+	pid=$(cat /tmp/$DAEMON.pid)
+	# Set TERM
+	kill -SIGTERM "${pid}"
+	# Wait for exit
+	wait "${pid}"
+	# All done.
+	echo "> Done... $?"
 }
 
 echo "> Running Jupyter-lab 🐍"
@@ -64,29 +63,29 @@ echo "> Running as $(id)"
 echo "> Parameters: $*"
 echo "> Jupyter options: $JUPYTER_OPT"
 
-if [ "$(basename "$1" 2> /dev/null)" == "$DAEMON" ]; then
+if [ "$(basename "$1" 2>/dev/null)" == "$DAEMON" ]; then
 
-    echo "> Starting $* $JUPYTER_OPT"
-    trap stop SIGINT SIGTERM
-    "$@" "${JUPYTER_OPT}" &
-    pid="$!"
-    echo $pid > /tmp/$DAEMON.pid
-    echo "> $DAEMON pid: $pid"
-    wait "${pid}"
-    exit $?
+	echo "> Starting $* $JUPYTER_OPT"
+	trap stop SIGINT SIGTERM
+	"$@" "${JUPYTER_OPT}" &
+	pid="$!"
+	echo $pid >/tmp/$DAEMON.pid
+	echo "> $DAEMON pid: $pid"
+	wait "${pid}"
+	exit $?
 
-elif echo "$*" | grep ^-- ; then
-    # accept parameters from command line or compose
-    echo "> Starting $* $JUPYTER_OPT"
-    trap stop SIGINT SIGTERM
-    jupyter-lab --no-browser --ip=0.0.0.0 "${JUPYTER_OPT}" "$@" &
-    pid="$!"
-    echo "$pid" > /tmp/"$DAEMON".pid
-    echo "> $DAEMON pid: $pid"
-    wait "${pid}"
-    exit $?
+elif echo "$*" | grep ^--; then
+	# accept parameters from command line or compose
+	echo "> Starting $* $JUPYTER_OPT"
+	trap stop SIGINT SIGTERM
+	jupyter-lab --no-browser --ip=0.0.0.0 "${JUPYTER_OPT}" "$@" &
+	pid="$!"
+	echo "$pid" >/tmp/"$DAEMON".pid
+	echo "> $DAEMON pid: $pid"
+	wait "${pid}"
+	exit $?
 else
-    # run command from docker run
-    echo "> Starting $* "
-    exec "$@" 
+	# run command from docker run
+	echo "> Starting $* "
+	exec "$@"
 fi
