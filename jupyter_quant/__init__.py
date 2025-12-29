d3a2e34e3590b5ed8be004e0e7f1dc0d65d950ff
gnzsnz <8376642+gnzsnz@users.noreply.github.com>
2025-12-29 22:11:19 +0100

    Refactor version management and update dependencies in project configuration

--- jupyter_quant/__init__.py
+++ jupyter_quant/__init__.py
@@ -1,3 +1,11 @@
 """Jupyter quant, a dockerized quant research environment."""
 
-__version__="2512.2"
+import re
+from importlib.metadata import version
+
+__version__ = version("cboe_toolz")
+
+# Extract only the numeric parts for __version_info__
+__version_info__ = tuple(
+    [int(x) for x in re.findall(r"\d+", __version__.split("+")[0])]
+)
